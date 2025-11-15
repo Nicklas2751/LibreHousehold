7522673aadf99d0d01fb3d6ad53aa3ecf3b8fcb2
Nicklas Wiegandt <nicklas@wiegandt.eu>
2025-11-15 23:36:59 +0100

    Update layout styles to improve spacing and overflow handling

--- frontend/src/routes/app/+layout.svelte
+++ frontend/src/routes/app/+layout.svelte
@@ -6,7 +6,7 @@
 
 <AppMenu/>
 
-<div class="md:flex md:justify-around">
+<div class="md:flex md:justify-around [padding-bottom:calc(4rem+env(safe-area-inset-bottom))] overflow-auto">
     <div class="md:max-w-[1200px] md:min-w-[1200px]">
         {@render children()}
     </div>
