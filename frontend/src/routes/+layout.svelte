9788a41c0b312d7e5ff127c7f14fc64805d7d5e7
Nicklas Wiegandt <nicklas@wiegandt.eu>
2026-03-21 19:53:43 +0100

    Refactor SetupWizard button click handler and enhance layout rendering with page key
    This is a workaround for the svelte bug sveltejs/svelte#17982

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
     import '../app.css';
     import favicon from '$lib/assets/favicon.svg';
     import Toasts from "$lib/Toasts.svelte";
+    import { page } from '$app/state';
 
     let {children} = $props();
 </script>
@@ -11,6 +12,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 </svelte:head>
 
-{@render children()}
+{#key page.url.pathname}
+    {@render children()}
+{/key}
 
 <Toasts />
